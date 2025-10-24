696bda7daeb154f9370fb2b3496dab5154268096
Nurlan I. Ibrahimov <Nurlan.Ib@bestcomp.net>
2025-10-24 18:58:32 +0400

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,50 @@
+import type React from "react"
+import type { Metadata } from "next"
+import { Geist } from "next/font/google"
+import { Geist_Mono } from "next/font/google"
+import { Toaster } from "@/components/ui/toaster"
+import { Suspense } from "react"
+import { LanguageProvider } from "@/contexts/language-context"
+import "flag-icons/css/flag-icons.min.css"
+
+import "./globals.css"
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+})
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+})
+
+export const metadata: Metadata = {
+  title: "Cybersoft",
+  description: "Cybersoft designs high-impact websites and scalable SaaS products that help businesses grow.",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className="font-sans antialiased">
+        <LanguageProvider>
+          <Suspense fallback={null}>
+            {children}
+            <Toaster />
+          </Suspense>
+        </LanguageProvider>
+      </body>
+    </html>
+  )
+}
