0d7ed9200aee081c0148d40205049857aa77d6f3
Nu2lan <nunuib64@gmail.com>
2025-11-04 21:57:02 +0400

    Initial Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,25 +47,6 @@ export default function RootLayout({
             <Toaster />
           </Suspense>
         </LanguageProvider>
-
-        {/* ✅ Tawk.to Live Chat Widget */}
-        <Script
-          id="tawk-to"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-              (function(){
-                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-                s1.async=true;
-                s1.src='https://embed.tawk.to/6908f637ca7409194f4ecbf0/1j95hn8el';
-                s1.charset='UTF-8';
-                s1.setAttribute('crossorigin','*');
-                s0.parentNode.insertBefore(s1,s0);
-              })();
-            `,
-          }}
-        />
       </body>
     </html>
   );
